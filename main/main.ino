23fb9d648a382b07e1a6a7a29e444ee6053d8f6a
froggoten <61162038+froggoten@users.noreply.github.com>
2020-03-08 16:06:06 +0100

    Update main.ino

--- main/main.ino
+++ main/main.ino
@@ -20,6 +20,51 @@ color Blanc = {0, 0, 0, 255};
 
 
 
+HSLtoRGB(H, S ,L );
+{
+  
+  
+}
+
+RGBtoHSL(R, G ,B );
+{
+  r=R/255;
+  g=G/255;
+  b=B/255;
+  m=min(min(r, g), b);
+  M=max(max(r, g),b);
+  
+ //luminescence
+  L=(m+M)/2;// arrondir?
+ // Saturation
+if (m==M) {
+  S=0;
+}
+else if (L<=0.5) {
+    S=(M-m)/(M+m);
+
+}
+else {
+  S=(M-m)/(2-M-m);
+}
+  
+ //Hue
+if (M==r) {
+  H=(g-b)/(M-m);
+}
+else if (M==g) {
+  H=2+(b-r)/(M-m);
+}
+else {
+  H=4+(r-g)/(M-m);
+}  
+ H=H*60 //arrondir?   
+  
+return(H, S, L)
+}
+
+
+
 
 Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRBW + NEO_KHZ800);
 #define DELAYVAL 100
